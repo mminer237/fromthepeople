b73d2cb5395a6bf81e1c4deb16ed9d571e34f5d3
Matthew Miner <mminer237@gmail.com>
2021-10-07 15:32:40 -0500

    Add header icon info

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import { Link } from "gatsby"
 import "./style.scss"
 import Seo from "../components/seo"
@@ -7,6 +8,15 @@ export default function Layout({ children, description, title }) {
 	return (
 		<>
 			<Seo description={description} title={title} />
+			<Helmet>
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#083c91" />
+				<meta name="msapplication-TileColor" content="#083c91" />
+				<meta name="theme-color" content="#083c91" />
+			</Helmet>
 			<header>
 				<Link to="/">
 					<h1><span class="from">From</span> <span class="the">the</span> <span class="people">People</span></h1>
