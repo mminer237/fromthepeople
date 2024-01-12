e17e0c646f5b537e25d03d1b5d31f1c36abcb7ad
Matthew Miner <mminer237@gmail.com>
2024-01-12 14:34:04 -0600

    Get Sankey chart actually drawing and updating

--- src/components/analysis.js
+++ src/components/analysis.js
@@ -13,72 +13,80 @@ export default function Analysis({ increase, targetYear = new Date().getFullYear
 	const maxPaid = Math.round(Data.incomeTaxes.years[incomeTaxYear].totalTaxPaid[Data.incomeTaxes.years[incomeTaxYear].totalTaxPaid.length - 1] / Data.incomeTaxes.years[incomeTaxYear].numbers[Data.incomeTaxes.years[incomeTaxYear].totalTaxPaid.length - 1]);
 	const maxTax = Math.log(maxPaid) + Math.log(maxPaid * percentIncrease);
 
+	let data = {
+		"nodes": [
+			{"node": 0, "name": "foo", "color": "#ffaa11"},
+			{"node": 1, "name": "bar", "color": "#ffaa55"},
+			{"node": 2, "name": "baz", "color": "#ffaa99"}
+		],
+		"links": [
+			{"source":0, "target":2, "value":2},
+			{"source":1, "target":2, "value":2}
+		]
+	};
+	const graph = sankey();
+	graph.nodeWidth(20);
+	graph.nodePadding(20);
+	// graph.extent([[15, 15], [500 - 15, 500 - 15]]);
+	graph.size([500, 500]);
+	const { nodes, links } = graph(data);
+	console.log(data);
+	console.log(graph);
+	console.log(nodes);
+	console.log(links);
+
 	useEffect(() => {
 		const svg = d3.select(".analysis > svg");
-		const data = {
-			"nodes": [
-				{"node": 0, "name": "foo"},
-				{"node": 1, "name": "bar"},
-				{"node": 2, "name": "baz"}
-			],
-			"links": [
-				{"source":0, "target":2, "value":2},
-				{"source":1, "target":2, "value":2}
-			]
-		};
-		const graph = sankey();
-		graph.nodeWidth(15);
-		graph.nodePadding(5);
-		graph.size([500, 500]);
-		const { nodes, links } = graph(data);
-		console.log(data);
-		console.log(graph);
-		console.log(nodes);
-		console.log(links);
-		// console.log(graph.nodes);
-		// console.log(graph.links());
-		// console.log(sankey().nodes(graph));
 
-		console.log(sankeyLinkHorizontal());
-		// add in the links
-		var link = svg.append("g")
-			.selectAll()
+		/* Add links */
+		svg.select("#links")
+			.selectAll("path")
 			.data(links)
-			.enter()
-			.append("path")
+			.join("path")
 				.attr("class", "link")
 				.attr("d", sankeyLinkHorizontal())
-				// .attr("d", sankeyLinkHorizontal())
-				.style("stroke-width", function(d) { return Math.max(1, d.dy); })
-				.sort(function(a, b) { return b.dy - a.dy; });
+				.style("stroke", d => d.source.color)
+				.style("stroke-width", d => d.width)
+				.sort((a, b) => b.dy - a.dy);
 		
-		// add in the nodes
-		var node = svg.append("g")
+		/* Add nodes */
+		svg.select("#nodes")
 			.selectAll(".node")
 			.data(nodes)
-			.enter().append("g")
-				.attr("class", "node")
-				.attr("transform", function(node) { console.log(node); return "translate(" + node.x0 + "," + node.y0 + ")"; });
-
-		// add the rectangles for the nodes
-		node
-			.append("rect")
-			.attr("height", graph.nodeWidth())
+			.join("rect")
+			.attr("class", "node")
+			.attr("x", node => node.x0)
+			.attr("y", node => node.y0)
+			.attr("height", node => node.y1 - node.y0)
 			.attr("width", graph.nodeWidth())
-			.style("fill", "#ffaa11")
-			.style("stroke", function(node) { return d3.rgb(node.color).darker(2); })
-			// Add hover text
-			.append("title")
-			.text(function(node) { return node.name + "\n" + "There is " + node.value + " stuff in this node"; });
-		
-		// return () => {
-		// 	svg.selectChildren().remove();
-		// }
-	}, []);
+			.style("fill", node => node.color)
+			.style("stroke", node => d3.rgb(node.color).darker(2));
+
+		/* Add node labels */
+		svg.select("#labels")
+			.selectAll("text")
+			.data(nodes)
+			.join("text")
+				.attr("x", node => node.x0 < graph.size()[0] / 2 ?
+					node.x1 + 6 :
+					node.x0 - 6
+				)
+				.attr("text-anchor", node => node.x0 < graph.size()[0] / 2 ?
+					"start" :
+					"end"
+				)
+				.attr("y", node => (node.y1 + node.y0) / 2)
+				.attr("dy", "0.35em")
+				.text(node => node.name);
+	}, [data]);
 
 	return (
 		<div className="analysis">
-			<svg></svg>
+			<svg>
+				<g id="links"></g>
+				<g id="nodes"></g>
+				<g id="labels"></g>
+			</svg>
 		</div>
 	)
 }
